e180a3ea5359850d55ab8944a5d9cd17510e7109
Reddlake <reddlake4eva@gmail.com>
2022-04-21 13:35:53 -0300

    Copied all the data for inventors as well as people

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ARRAY-CARDIO-DAY-1
+#
+# My goal for this JS-30 project is to progress with javascript fundamentals.
---
+++ index.js
@@ -0,0 +1,17 @@
+const inventors = [
+    {first: 'Albert', last: 'Einstein', year: 1879, passed: 1955},
+    {first: 'Isaac', last: 'Newton', year: 1643, passed: 1727},
+    {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642},
+    {first: 'Marie', last: 'Curie', year: 1867, passed: 1934},
+    {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630},
+    {first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543},
+    {first: 'Max', last: 'Planck', year: 1858, passed: 1947},
+]
+
+const people = ['Back, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
+'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire',
+'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas',
+'Bentsen, Lloyd', 'Berger, Rick', 'Bergman, Ingmar', 'Berio, Luciano',
+'Berle, Milton', 'Berline, Irving', 'Berne, Eric', 'Bevan, Aneurin', 'Bevel, Ken',
+'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh',
+'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Black, William'];
